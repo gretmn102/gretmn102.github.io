b8fffa5bcaae15a95502a711069fe2e20dd208c9
gretmn102 <erlgreywatson@gmail.com>
2024-01-09 15:29:21 +0200

    change edit urls

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/gretmn102/gretmn102.github.io/edit/main/',
           remarkPlugins: [remarkBreaks],
         },
         blog: {
@@ -47,7 +47,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/gretmn102/gretmn102.github.io/edit/main/',
           remarkPlugins: [remarkBreaks],
         },
         theme: {
