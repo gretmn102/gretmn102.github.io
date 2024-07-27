dbb056ecd4eb37ded4d0b25d21a2b88d0bafe3e6
Fering <treesortbubble@gmail.com>
2024-07-27 18:49:37 +0300

    feat: draw my own social card (#14)
    Closes: #11

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/agent-social-card.png',
     navbar: {
       title: 'Агент Лапкин',
       logo: {
